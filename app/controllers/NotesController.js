6af5929fe22136c75db649663a3d949b6effb7ff
Alex Spiegel <alexspiegel28@gmail.com>
2024-02-09 14:59:06 -0700

    Added code for created new note

--- app/controllers/NotesController.js
+++ app/controllers/NotesController.js
@@ -19,6 +19,28 @@ export class NotesController {
     constructor() {
         console.log('Notes loaded')
 
+        // notesService.loadNotesFromLocalStorage() this will be used later. added for now
+
         _drawNotes()
+
+        AppState.on('notes', _drawNotes)
+    }
+
+    createNote() {
+        try{
+            event.preventDefault()
+            console.log('creating note');
+
+            const form =event.target
+
+            console.log('targeted event', form);
+
+            const noteFormData = getFormData(form)
+
+            console.log('here is your note object', noteFormData)
+
+            notesService.createNote(carFormData)
+
+        }
     }
 }
